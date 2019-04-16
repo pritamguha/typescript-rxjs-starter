423965a4d80694211f0648bf611d514309980b7e
pritamguha <pritamguha26@gmail.com>
2019-04-16 23:10:14 +0530

    multiple subscriptions, error-handling with try-catch block

--- index.ts
+++ index.ts
@@ -1,11 +1,35 @@
 // Import stylesheets
 import './style.css';
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 
 let observable = Observable.create((observer: any) => {
-  observer.next('Hey guys!')
+  try {
+    observer.next('Hey guys!')
+    observer.next('How are you?')
+    observer.complete()
+    observer.next('This will not send')
+  } catch (err) {
+    observer.error(err);
+  }
+
+
 })
 
-observable.subscribe((res: any) => {
-console.log(res);
-})
+observable.subscribe(
+  (res: any) => {
+    addItem(res)
+  },
+  (error: any) => {
+    addItem(error);
+  },
+  () => {
+    addItem('Completed')
+  })
+
+function addItem(val: any) {
+  var node = document.createElement('li');
+  var textNode = document.createTextNode(val);
+  console.log(node);
+  node.appendChild(textNode);
+  document.getElementById('output').appendChild(node);
+}
